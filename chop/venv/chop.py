90175d870acc319d0c2b7bfe919e143e13c0a90c
ceceoo <xinyizhangce@outlook.com>
2019-07-04 16:29:29 +0800

    single value

--- chop/venv/chop.py
+++ chop/venv/chop.py
@@ -1,7 +1,16 @@
 def chop_list(array, target):
     if len(array) == 0:
         return chop_empty_list(array, target)
+    elif len(array) == 1:
+        return chop_single_value_list(array, target)
 
 
 def chop_empty_list(array, target):
     return -1
+
+
+def chop_single_value_list(array, target):
+    if array[0] == target:
+        return 0
+    else:
+        return -1
--- chop/venv/chopTest.py
+++ chop/venv/chopTest.py
@@ -7,6 +7,8 @@ class MyTestCase(unittest.TestCase):
         empty_list = []
         self.assertEqual(chop_list(empty_list, 1), -1)
 
-
+    def test_single_value_list(self):
+        single_value = [3]
+        self.assertEqual(chop_list(single_value, 3), 0)
 if __name__ == '__main__':
     unittest.main()
