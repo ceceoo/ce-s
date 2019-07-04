33de6921c8a2b5d91dcaf04e1e6fb42795a3d790
ceceoo <xinyizhangce@outlook.com>
2019-07-04 16:37:24 +0800

    multivalue

--- chop/venv/chop.py
+++ chop/venv/chop.py
@@ -3,6 +3,8 @@ def chop_list(array, target):
         return chop_empty_list(array, target)
     elif len(array) == 1:
         return chop_single_value_list(array, target)
+    else:
+        return chop_multi_value_list(array, target)
 
 
 def chop_empty_list(array, target):
@@ -14,3 +16,18 @@ def chop_single_value_list(array, target):
         return 0
     else:
         return -1
+
+
+def chop_multi_value_list(array, target):
+    start = 0
+    end = len(array) - 1
+    while start <= end:
+        middle = int((start+end) / 2)
+        if array[middle] > target:
+            end = middle - 1
+        elif array[middle] < target:
+            start = middle + 1
+        else:
+            return middle
+    return -1
+
--- chop/venv/chopTest.py
+++ chop/venv/chopTest.py
@@ -10,5 +10,11 @@ class MyTestCase(unittest.TestCase):
     def test_single_value_list(self):
         single_value = [3]
         self.assertEqual(chop_list(single_value, 3), 0)
+
+    def test_multi_value_list(self):
+        multi_value = [1,34,56,88,100]
+        self.assertEqual(chop_list(multi_value, 34), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
